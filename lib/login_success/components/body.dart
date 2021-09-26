2db98e9c0855f16a738ea34445654e98d4ece966
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2021-09-26 21:40:58 +0530

    login success screen added

---
+++ lib/login_success/components/body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:socialbennett/components/default_button.dart';
+import 'package:socialbennett/screens/home/home_screen.dart';
+import 'package:socialbennett/size_config.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  void initState() {
+    new Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Lottie.asset("assets/animation/success.json", repeat: false),
+    );
+  }
+}
---
+++ lib/login_success/login_success_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class LoginSuccessScreen extends StatelessWidget {
+  static String routeName = "/login_success";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: SizedBox(),
+      ),
+      body: Body(),
+    );
+  }
+}
